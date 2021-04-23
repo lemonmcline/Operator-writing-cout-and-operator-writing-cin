83deda4017f239166a3a48c50683e5cca8fbdd51
Artyom Pishalnikov <82143842+lemonmcline@users.noreply.github.com>
2021-04-23 12:17:38 +0600

    Operator-writing-cout-and-operator-writing-cin
    Operator-writing-cout-and-operator-writing-cin

---
+++ Operator writing cout and operator writing cin.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{  
+    setlocale(0, "");
+    int z;
+    cout << "Введите число" << endl;
+    cin >> z;
+    cout << "Число подтверждено " << endl;
+
+    cout << endl;
+
+    string x;
+    cout << "Введите строку" << endl;
+    cin >> x;
+    cout << "Строка подтверждена" << endl;
+
+    cout << endl;
+
+    float c;
+    cout << "Введите дробное число" << endl;
+    cin >> c;
+    cout << "Дробно число подтверждено" << endl;
+}
